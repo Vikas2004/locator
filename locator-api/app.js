071333fb5eae1b0c100fabb909207b9ef9171c57
Deekshith Maram <s538365@nwmissouri.edu>
2021-03-01 00:17:04 -0600

    connect api to mongodb

--- locator-api/app.js
+++ locator-api/app.js
@@ -1,8 +1,19 @@
 var express = require('express');
 var app = express();
+var env = require('dotenv').config()
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
-});
+const MongoClient = require('mongodb').MongoClient  
+
+
+const client = new MongoClient('mongodb+srv://vikas2005:vikas2005@locator.mkp5q.mongodb.net/locator?retryWrites=true&w=majority', { useNewUrlParser: true });
+client.connect((err, database) => {
+  console.log("--goes to db--")
+  if (err) return console.log(err);
+  var database = database.db("locator");
+ app.listen(3000, function () {
+   return "Connected to Database"
+   
+ })
+})
