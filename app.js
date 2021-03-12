5c5b02834cc1521277db939825956aae29dba6e0
Deekshith Maram <s538365@nwmissouri.edu>
2021-03-12 04:15:01 -0600

    remove logs

--- app.js
+++ app.js
@@ -9,19 +9,6 @@ var bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// const MongoClient = require('mongodb').MongoClient  
-
-
-// const client = new MongoClient(process.env.ATLAS_URI, { useNewUrlParser: true });
-// client.connect((err, database) => {
-//   console.log("--goes to db--")
-//   if (err) return console.log(err);
-//   var database = database.db("locator");
-//  app.listen(3000, function () {
-//    return "Connected to Database"
-   
-//  })
-// })
 
 mongoose.connect(process.env.ATLAS_URI, { useNewUrlParser: true, useUnifiedTopology: true }).then((res) =>{
    app.listen(3000, function () {
