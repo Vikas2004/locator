72d01fbf5f1c485412410e2ec51dbd5d4f8d57ed
Deekshith Maram <s538365@nwmissouri.edu>
2021-03-17 05:23:27 -0500

    show locations

--- locator-client/src/views/screens/Location/saveLocation.js
+++ locator-client/src/views/screens/Location/saveLocation.js
@@ -1,20 +1,95 @@
 
 import './location';
+import React, {Component} from 'react'
+import axios from 'axios'
 import Header from '../../components/Header'
 import Footer from '../../components/Footer'
 // import ColorBox from '../../components/ColorBox'
 
-function SaveLocation() {
-  return (
-    <div>
-      <Header/>
-      <div style={{display: "flex", alignItems: "center", background: "white", height: "100vh"}}>
-      <span style={{fontSize: 20, color: "black", width: "100%"}}>Save the Locations</span>
+export default class SaveLocation extends Component {
+
+  constructor(){
+    super()
+  this.state = {
+    locationName: '',
+    latitude: '',
+    longitude: '',
+  }
+  this.handleSubmit = this.handleSubmit.bind(this)
+  this.handleNameChange = this.handleNameChange.bind(this)
+  this.handleLatitudeChange = this.handleLatitudeChange.bind(this)
+  this.handleLongitudeChange = this.handleLongitudeChange.bind(this)
+  }
+
+  componentDidMount(){
+     
+  }
+
+  handleNameChange(event) {
+    console.log(event,"--event is here--")
+    this.setState({locationName: event.target.value});
+  }
+
+  
+  handleLatitudeChange(event) {
+    console.log(event,"--event is here--")
+    this.setState({latitude: event.target.value});
+  }
+
+    
+  handleLongitudeChange(event) {
+    console.log(event,"--event is here--")
+    this.setState({longitude: event.target.value});
+  }
+
+  async handleSubmit(){
+    const headers = {
+      'content-type': 'application/x-www-form-urlencoded',
+    }
+    const body = {
+      locationName: this.state.locationName,
+      latitude: this.state.latitude,
+      Longitude: this.state.longitude
+    }
+    console.log(body,"--body is here--")
+   await axios.post('http://localhost:3000/location/', body, { headers }).then(function (response) {
+      console.log(response,"----response is here---");
+    })
+    .catch(function (error) {
+      console.log(error,"---error is here---");
+    });
+
+  };
+  
+
+
+  render(){
+    return (
+      <div>
+        <Header/>
+        <div style={{display: "flex", alignItems: "center", background: "white", height: "100vh"}}>
+        <form onSubmit={this.handleSubmit}>
+        <label>
+          Location Name:
+          <input type="text" value={this.state.locationName} onChange={this.handleNameChange} />
+        </label>
+        <label>
+          Latitude:
+          <input type="text" value={this.state.latitude} onChange={this.handleLatitudeChange} />
+        </label>
+        <label>
+          Longitude:
+          <input type="text" value={this.state.longitude} onChange={this.handleLongitudeChange} />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+        </div>
+        
+        <Footer />
       </div>
-      
-      <Footer />
-    </div>
-  );
+    );
+  }
+ 
 }
 
-export default SaveLocation;
+
