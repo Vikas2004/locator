72b8abf8e8c3fe97bb7e1f9700e1df4be1a24c0f
Vikas2004 <S538336@NWMISSOURI.EDU>
2021-03-19 01:27:18 -0500

    add headers

--- controllers/locationController.js
+++ controllers/locationController.js
@@ -3,6 +3,12 @@
 const express=require('express')
 const app=express.Router()
 var LocationSchema = require('../models/location')
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next()
+})
 
 // let bodyParser = require('body-parser');
 
