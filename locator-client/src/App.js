ddf0832b4978f00dbe7b5e7d1a54d786b3792471
Deekshith Maram <s538365@nwmissouri.edu>
2021-03-19 09:40:24 -0500

    about added

--- locator-client/src/App.js
+++ locator-client/src/App.js
@@ -5,6 +5,7 @@ import Location from '../src/views/screens/Location/location'
 import SaveLocation from '../src/views/screens/Location/saveLocation'
 import NotFound from '../src/views/components/NotFound/NotFound'
 import EditLocation from '../src/views/screens/Location/editLocation'
+import About from '../src/views/screens/Location/about'
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
       <Route path="/" component={Treasure} exact />
       <Route path="/locations/" component={Location} exact/>
       <Route path="/locations/save" component={SaveLocation} exact />
+      <Route path="/locations/about" component={About} exact />
       <Route path="/locations/edit" component={EditLocation} exact />
       <Route component={NotFound} />
       </Switch>
